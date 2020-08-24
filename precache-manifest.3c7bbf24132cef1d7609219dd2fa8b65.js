8a24ae4c87d0b5985a586f0e1aaba83a2dd41c9f
Daniel <danielnaizabekov@gmail.com>
2020-08-24 13:28:37 +0600

    Updates

---
+++ precache-manifest.3c7bbf24132cef1d7609219dd2fa8b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7dd2cbb1ca489903443e873ad63ba1",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "a19d39fd14c9ef971215",
+    "url": "/dist/static/css/main.fc901cdc.chunk.css"
+  },
+  {
+    "revision": "d8fbaa218ccbdadc5226",
+    "url": "/dist/static/js/2.64d8546f.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/dist/static/js/2.64d8546f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19d39fd14c9ef971215",
+    "url": "/dist/static/js/main.0c3dfaf0.chunk.js"
+  },
+  {
+    "revision": "2bef558abf73bcfa2e81",
+    "url": "/dist/static/js/runtime-main.58342a5d.js"
+  }
+]);
